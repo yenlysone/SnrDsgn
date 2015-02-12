69c32ffa31890f6e5832e0e29e4424dd716d5fd9
3lixir <landry1nda@gmail.com>
2015-02-12 03:57:04 -0600

    Beta Test |  I'm able to access the repo correctly.

---
+++ src/Fold1/landryhasaccess.java
@@ -0,0 +1,23 @@
+package Fold1;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class landryhasaccess {
+
+	public static void popup() {
+
+
+		    JFrame.setDefaultLookAndFeelDecorated(true);
+		    JFrame frame = new JFrame();
+		    frame.setTitle("SENIOR DESIGN");
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    JLabel label = new JLabel("LANDRY HAS ACCESS LET'S START");
+		    frame.add(label);
+		    frame.pack();
+		    frame.setVisible(true);
+		  
+		
+	}
+
+}
